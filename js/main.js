97317418f631ff053c88cc7319e55c1ce7e9c77a
Nyeem Davis <nyeemdavis4@gmail.com>
2023-06-23 05:19:50 -0400

    function to remove default action of anchor

---
+++ js/main.js
@@ -0,0 +1,13 @@
+anchors = document.querySelectorAll('a[href^="#"]');
+// This function is used to scroll to the section when clicking on the anchor, also prevent the default behaviour of the anchor
+anchors.forEach(function(anchor) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        var target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+        });
+    });
+
+});
