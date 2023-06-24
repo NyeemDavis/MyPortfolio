187016c254376dd578df7739944dac6e3466d7ba
Nyeem Davis <nyeemdavis4@gmail.com>
2023-06-24 06:30:14 -0400

    changed to regular func

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-anchors = document.querySelectorAll('a[href^="#"]');
+anchors = document.querySelectorAll('.header-anchor[href^="#"');
 // This function is used to scroll to the section when clicking on the anchor, also prevent the default behaviour of the anchor
 anchors.forEach(function(anchor) {
-    anchor.addEventListener('click',(e) => {
+    anchor.addEventListener('click',function(e) {
         e.preventDefault();
         var target = document.querySelector(this.getAttribute('href'));
         target.scrollIntoView({
